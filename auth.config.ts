36551ce80faeeff414f5754101d77077eac20484
Liam Darkmoon <liamdarkmoon@gmail.com>
2024-03-22 14:33:32 -0300

    mw

--- auth.config.ts
+++ auth.config.ts
@@ -8,14 +8,8 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnLanding = nextUrl.pathname.startsWith('/');
-      if (isOnLanding) {
         if (isLoggedIn) return true;
         return false; 
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/', nextUrl));
-      }
-      return true;
     },
   },
   providers: [], 
